7a76ced2c79c63e67726a405d1a2250c9c941691
necixy-gmail <96526913+necixy-gmail@users.noreply.github.com>
2022-01-13 00:58:04 +0530

    -updates description condition

--- src/payments/schema/transaction.schema.ts
+++ src/payments/schema/transaction.schema.ts
@@ -6,6 +6,7 @@ export const TransactionSchema = new mongoose.Schema(
     transactionId: { type: String, unique: true },
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     meta: Object,
+    description: String
   },
   {
     timestamps: true,
